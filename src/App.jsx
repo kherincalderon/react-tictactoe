8624d3d719e1ef69dd1ca682cc599d049e38f611
kherin.eth <kherincalderon@gmail.com>
2022-05-22 22:50:57 -0600

    [init] project

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import TicTacToe from "./components/TicTacToe";
+
+function App() {
+  return (
+    <main>
+      <TicTacToe />
+    </main>
+  );
+}
+
+export default App;
